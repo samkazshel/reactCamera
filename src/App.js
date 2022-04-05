e20c37182f80afc1d6356a57450414070cf10d36
samkazshel <samsheldon779@gmail.com>
2022-04-05 16:10:51 +0100

    adding method that should display the image uploaded (currently not working)

--- src/App.js
+++ src/App.js
@@ -3,10 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  var loadFile = function(event){
+    var image = document.getElementById('output');
+    image.src = URL.createObjectURL(event.target.files[0]);
+  }
   return (
     <div className="App">
-      <input accept='image/*' id="icon-button-file" type="file"
-        capture="environment" />
+      <p><label for="file"> Take Image </label></p>
+      {/* <input accept='image/*' id="icon-button-file" type="file"
+        capture="environment" /> */}
+        <p><img id="output" width="200"/></p>
+        <input type="file" accept='image/*' name='image' id='file' onChange={loadFile} style="display: none;"></input>
     </div>
   );
 }
